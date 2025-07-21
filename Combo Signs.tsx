0e9152e2c7e203c5d3ba644b5fe960a43f929e9b
Gabriele Magni <gabri.magni.97@gmail.com>
2025-07-21 17:15:48 +0200

    nuovo interno, libreria piu corta

--- Combo Signs.tsx
+++ Combo Signs.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Combo Signs" tilewidth="12" tileheight="12" tilecount="12" columns="4">
- <image source="imgs/signs-combo.png" width="48" height="36"/>
+ <image source="public/assets/imgs/signs-combo.png" width="48" height="36"/>
 </tileset>
