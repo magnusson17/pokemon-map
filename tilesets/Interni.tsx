4f2d8c91ba7f199b8568fe8ffa7d07f181d81383
Gabriele Magni <gabri.magni.97@gmail.com>
2025-07-23 15:42:49 +0200

    modifiche interno faro

--- tilesets/Interni.tsx
+++ tilesets/Interni.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Interni" tilewidth="12" tileheight="12" tilecount="90" columns="18">
- <image source="../public/assets/imgs/interni.png" width="216" height="60"/>
+<tileset version="1.10" tiledversion="1.11.2" name="Interni" tilewidth="12" tileheight="12" tilecount="285" columns="19">
+ <image source="../public/assets/imgs/interni.png" width="239" height="180"/>
 </tileset>
